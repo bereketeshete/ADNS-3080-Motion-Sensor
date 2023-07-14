d0ffd46065f8a2270adb7c5d2aeac53bb0f61797
Bereket Eshete <40177156+bereketeshete@users.noreply.github.com>
2023-07-14 12:29:44 -0500

    regular update

--- ADNS_3080_DR.ino
+++ ADNS_3080_DR.ino
@@ -1,6 +1,5 @@
 //#include <LiquidCrystal.h>
 
-
 // written by Bereket Kebede
 // Dextrous Robotics
 /*
@@ -8,9 +7,7 @@
  */
 //#include <ADNS3080.h>
 
-
 // Uses ATPMEGA328 Old Bootloader
-
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -44,8 +41,6 @@
 #define ADNS3080_PIXEL_BURST          0x40
 #define ADNS3080_MOTION_BURST         0x50
 #define ADNS3080_PRODUCT_ID_VALUE     0x17
-
-
 
 
 //--------------- Template Parameters ---------------- [ No characters after backlash! ]
@@ -103,21 +98,30 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,20,4); //Change to (0x27,16,2) fo
 // Define variables:
 long duration;
 int distance;
+int count_freq =0;
 
 
 ADNS3080 <PIN_RESET, PIN_CS> sensor;
 
 // Initial position
-int x = 0;
-int y = 0;
+int8_t x = 0; // for sensor.motion burst
+int8_t y = 0;
 int count = 0;
 unsigned long StartTime;
+unsigned long CurrentTime;
+unsigned long ElapsedTime ;
 
+int8_t lock = 0;
+
+
+// Initial position
+int xx = 0; // for sensor.displacement burst
+int yy = 0;
 
 
 
 void setup() {
-  sensor.setup( LED_MODE, RESOLUTION );
+  sensor.setup(LED_MODE,RESOLUTION);
   Serial.begin(9600);
   pinMode(2, OUTPUT);
   //Initiate the LCD:
@@ -137,33 +141,58 @@ float mag(float dx, float dy){
   return d_avg;
 }
 
+int8_t magni(int8_t dx, int8_t dy){
+  int8_t d_avg = sqrt (pow (dx, 2) + pow (dy, 2));
+  return d_avg;
+}
+
 void occlusion_sensor(uint8_t max_pixel){
     if (max_pixel < 30) {
     digitalWrite(2, HIGH);
   } else {
+    // object infront of it
     digitalWrite(2, LOW);
   }
 }
 
 
+
+int convTwosComp(int b){
+  //Convert from 2's complement
+  if(b & 0x80){
+    b = -1 * ((b ^ 0xff) + 1);
+    }
+  return b;
+  }
+
+
 void loop() {
   uint8_t  motion;      // Did motion occur? true or false
-  int8_t   dx, dy;      // Displacement since last function call
+  int8_t   dx, dy;      // Displacement since last function call, for motion function
+  //int8_t   dxx, dyy;      // Displacement since last function call,, for displacement function
   uint8_t  squal;       //  Surface quality value - Larger value means better tracking. If below ~15 the displacement is dissabled. Adjust focus for 30 or more.
   uint16_t shutter;     // Camera shutter rate in clock cycles.
   uint8_t  max_pixel;   // Maximum pixel value of current frame (max brightness)
 
   char my_output_flag[] = "serial_digital_output";
+  unsigned long polling_freq;
+  
 
   sensor.motionBurst( &motion, &dx, &dy, &squal, &shutter, &max_pixel );
 
+ 
+  //sensor.displacement( &dxx, &dyy );
+
   // Integrate displacements
-  x += dx;
-  y += dy;
+  // x += dx;
+  // y += dy;
+
+  // xx += dxx;
+  // yy += dyy;
 
   //print magnitude and angle of movt
-  float average_dist = mag(dx,dy);
-  float my_angle = direction(dx,dy);
+  // float average_dist = mag(x,y);
+  // float my_angle = direction(dx,dy);
 
   ////////////////////////////////////////
   // option 2
@@ -211,30 +240,30 @@ void loop() {
 
 
   /////////////////////////////////////
-  // new
+  // new (June 26)
   
-  Serial.print("Average:");
-  Serial.println(average_dist);
-  Serial.print("angle:");
-  Serial.println(my_angle);
+  // Serial.print("Average:");
+  // Serial.println(average_dist);
+  // Serial.print("angle:");
+  // Serial.println(my_angle);
 
-  lcd.setCursor(0,0); // Set the cursor to column 1, line 1 (counting starts at zero)
-  lcd.print("count"); // Prints string "Display = " on the LCD
-  lcd.setCursor(0,1); // Set the cursor to column 1, line 1 (counting starts at zero)
-  //lcd.print(round(average_dist)); // Prints the measured distance
-  lcd.print(average_dist); // Prints the measured distance
+  // lcd.setCursor(0,0); // Set the cursor to column 1, line 1 (counting starts at zero)
+  // lcd.print("count"); // Prints string "Display = " on the LCD
+  // lcd.setCursor(0,1); // Set the cursor to column 1, line 1 (counting starts at zero)
+  // //lcd.print(round(average_dist)); // Prints the measured distance
+  // lcd.print(average_dist); // Prints the measured distance
 
-  delay(500);
+  // delay(500);
 
-  if (my_output_flag == "serial_digital_output"){
-     Serial.print("Average:");
-     Serial.println(average_dist);
-     Serial.print("angle:");
-     Serial.println(my_angle);
-  }
-  else {
-    Serial.println("choose correct output flag");
-  }
+  // if (my_output_flag == "serial_digital_output"){
+  //    Serial.print("Average:");
+  //    Serial.println(average_dist);
+  //    Serial.print("angle:");
+  //    Serial.println(my_angle);
+  // }
+  // else {
+  //   Serial.println("choose correct output flag");
+  // }
   /////////////////////////////////////////////////////////
 
 
@@ -292,5 +321,163 @@ void loop() {
   //   digitalWrite(2, LOW);
   // }
 
+
+  /////////////////////////////////////
+  // ouput 9
+
+  // occlusion_sensor(max_pixel);
+  // Serial.print("Average:");
+  // Serial.println(average_dist);
+  // Serial.print("angle:");
+  // Serial.println(my_angle);
+
+  // lcd.setCursor(0,0); // Set the cursor to column 1, line 1 (counting starts at zero)
+  // lcd.print("count:"); // Prints string "Display = " on the LCD
+  // lcd.print(average_dist); // Prints the measured distance
+  // lcd.setCursor(0,1); // Set the cursor to column 1, line 1 (counting starts at zero)
+  // lcd.print("squal:");
+  // lcd.print(squal);
+  // lcd.print(" max:");
+  // lcd.print(max_pixel);
+  // delay(500);
+
+  /////////////////////////////////////////////////////////
+  // output 10
+
+  // int8_t dxx = convTwosComp(dx);
+  // int8_t dyy = convTwosComp(dx);
+
+  // occlusion_sensor(max_pixel);
+  // // Serial.print("x:");
+  // // Serial.println(dxx);
+  // // Serial.print("y:");
+  // // Serial.println(dyy);
+
+  // Serial.print( " dx=" );
+  // Serial.print( dx );
+  // Serial.print( " dy=" );
+  // Serial.print( dy );
+  // Serial.print( " x=" );
+  // Serial.print( x );
+  // Serial.print( " y=" );
+  // Serial.print( y );
+  // Serial.print( " mag=" );
+  // Serial.print( y );
+  // Serial.println(magni(x,y));
+
+  // lcd.setCursor(0,0); // Set the cursor to column 1, line 1 (counting starts at zero)
+  // lcd.print("x:"); // Prints string "Display = " on the LCD
+  // lcd.print(dy); // Prints the measured distance
+  // lcd.print(" | y:"); // Prints string "Display = " on the LCD
+  // lcd.print(dy); // Prints the measured distance
+  // lcd.setCursor(0,1); // Set the cursor to column 1, line 1 (counting starts at zero)
+  // lcd.print("sq:");
+  // lcd.print(squal);
+  // lcd.print("|mag:");
+  // lcd.print(magni(x,y));
+  //delay(500);
+
+   /////////////////////////////////////////////////////////
+  // output 11
+
+  int8_t dxx = convTwosComp(dx);
+  int8_t dyy = convTwosComp(dy);
+
+  occlusion_sensor(max_pixel);
+  // Serial.print("x:");
+  // Serial.println(dxx);
+  // Serial.print("y:");
+  // Serial.println(dyy);
+
+  //float8_t = speed;
+  float speed = dx;
+
+  // Serial.print( " dx=" );
+  // Serial.print( dx );
+  // Serial.print( " dy=" );
+  // Serial.print( dy );
+  // Serial.print( " x=" );
+  // Serial.print( x );
+  // Serial.print( " y=" );
+  // Serial.print( y );
+  //Serial.print( " speed=" );
+  Serial.println(speed);
+
+  lcd.setCursor(0,0); // Set the cursor to column 1, line 1 (counting starts at zero)
+  lcd.print("speed:"); // Prints string "Display = " on the LCD
+  lcd.print(speed); // Prints the measured distance
+  lcd.print(" cm/s"); // Prints the measured distance
+  // lcd.setCursor(0,1); // Set the cursor to column 1, line 1 (counting starts at zero)
+  // lcd.print(" | y:"); // Prints string "Display = " on the LCD
+  // lcd.print(dy); // Prints the measured distance
+  // lcd.print("sq:");
+  // lcd.print(squal);
+  // lcd.print("|mag:");
+  // lcd.print(magni(x,y));
+  // 500 works best so far
+  //delay(500);
+
+  /////////////////////////////////////////////////////////
+  // output 12
+
+//   //occlusion_sensor(max_pixel);
+//   //float8_t = speed;
+//   float speed = dx/20.00;
+
+//   // Serial.print( " dx=" );
+//   // Serial.print( dx );
+//   // Serial.print( " dy=" );
+//   // Serial.print( dy );
+//   // Serial.print( " x=" );
+//   // Serial.print( x );
+//   // Serial.print( " y=" );
+//   // Serial.print( y );
+//   // Serial.print( " speed=" );
+//   // Serial.println(ElapsedTime);
+//   lcd.setCursor(0,0); // Set the cursor to column 1, line 1 (counting starts at zero)
+//   lcd.print("speed:"); // Prints string "Display = " on the LCD
+//   lcd.print(speed); // Prints the measured distance
+//   lcd.print(" cm/s"); // Prints the measured distance
+
+//   lcd.setCursor(0,1); // Set the cursor to column 1, line 1 (counting starts at zero)
+//   lcd.print("poll:"); // Prints string "Display = " on the LCD
+//   lcd.print(ElapsedTime); // Prints the measured distance
+//   lcd.print(" cm/s"); // Prints the measured distance
+//   //delay(200);
+
+
+//   CurrentTime = millis();
+
+//   // if (ElapsedTime < 1000){
+//   //  count++;
+//   //  Serial.println(ElapsedTime);
+//   // }
+
+//   unsigned long ElapsedTime = CurrentTime - StartTime;
+
+//    if (ElapsedTime < 1000){
+//    count++;
+//    Serial.println(ElapsedTime);
+//  }
+//  else {
+//    // return freq after one sec
+//    count_freq ++;
+//    CurrentTime = millis();
+//    ElapsedTime = CurrentTime - StartTime;
+//    Serial.print("frequency is: ");
+//    Serial.print(count);
+//    Serial.println(" Hz");
+//  }
+
+//   // if (lock == 0){
+//   //   lock = lock + 1;
+//   //   //ang_speed = 1/ElapsedTime;
+//   //   //polling_freq = 1.00/ (ElapsedTime/100000.00);
+//   //   // polling_freq = ElapsedTime;
+//   //   Serial.print("poll=" );
+//   //   //Serial.println(1000.00/(CurrentTime - StartTime), 5);
+//   //   //Serial.println(1000.00/(ElapsedTime), 5);
+//   //   Serial.println(ElapsedTime, 5);
+//   // }
 
 }
